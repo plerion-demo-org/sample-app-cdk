37b67fe6807a1c08938465582f2c63617d490ef6
Sam <sam.mirzaee@plerion.com>
2025-09-02 17:58:04 +1000

    feat: add vulnerable Lambda functions

---
+++ lib/sample-app-cdk-stack.ts
@@ -0,0 +1,42 @@
+import { Stack, StackProps, Tags, Duration, aws_lambda as lambda } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import * as path from 'path';
+
+export class SampleAppCdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const prefix = this.node.tryGetContext('resourcePrefix') ?? 'pd-';
+    const owner = this.node.tryGetContext('ownerTag') ?? 'PlerionDemo';
+
+    Tags.of(this).add('Owner', owner);
+
+    const commonFnProps = {
+      runtime: lambda.Runtime.NODEJS_22_X,
+      memorySize: 128,
+      timeout: Duration.seconds(10),
+      bundling: {
+        minify: false,
+        externalModules: [],
+        sourcesContent: false
+      }
+    } as const;
+
+    new NodejsFunction(this, 'LambdaA', {
+      ...commonFnProps,
+      functionName: `${prefix}lambda-a`,
+      entry: path.join(__dirname, '..', 'src', 'lambda-a', 'index.ts'),
+      handler: 'handler'
+    });
+
+    new NodejsFunction(this, 'LambdaB', {
+      ...commonFnProps,
+      functionName: `${prefix}lambda-b`,
+      entry: path.join(__dirname, '..', 'src', 'lambda-b', 'index.ts'),
+      handler: 'handler'
+    });
+  }
+}
+
+
